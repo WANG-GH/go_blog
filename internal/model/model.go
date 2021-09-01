d76407cf7f8840fad2ee5e6b32a23576b9f3e6c6
WANG-GH <281484683@qq.com>
2021-09-01 21:06:40 +0800

    test

---
+++ internal/model/model.go
@@ -0,0 +1,49 @@
+package model
+
+//const(
+//	STATE_OPEN = 1
+//	STATE_CLOSE = 0
+//)
+
+type Model struct{
+	ID uint32 `gorm:"primary_key" json:"id"`
+	CreatedBy string `json:"create_by"`
+	ModifiedBy string `json:"modified_by"`
+	CreatedOn  uint32 `json:"created_on"`
+	ModifiedOn uint32 `json:"modified_on"`
+	DeletedOn  uint32 `json:"deleted_on"`
+	IsDel      uint8  `json:"is_del"`
+}
+
+type Tag struct{
+	*Model
+	Name string `json:"name"`
+	State uint8 `json:"state"`
+}
+
+func (t Tag)TableName() string{
+	return "blog_tag"
+}
+
+type Article struct{
+	*Model
+	Title         string `json:"title"`
+	Desc          string `json:"desc"`
+	Content       string `json:"content"`
+	CoverImageUrl string `json:"cover_image_url"`
+	State         uint8  `json:"state"`
+}
+
+func (a Article) TableName() string{
+	return "blog_article"
+}
+
+type ArticleTag struct{
+	*Model
+	TagId uint32 `json:"tag_id"`
+	ArticleId uint32 `json:"article_id"`
+}
+
+func (a ArticleTag) TableName() string {
+	return "blog_article_tag"
+}
